288d035446d33464139200ce8ac3433a73bb0350
andrealanz <lanza@oregonstate.edu>
2019-07-25 15:23:32 -0700

    Added dashboard and P&L chart

--- shinyAppProject1/server.R
+++ shinyAppProject1/server.R
@@ -7,21 +7,22 @@ library(here)
 function(input, output) {
   
   # Filter data based on selections
-  output$table <- DT::renderDataTable(DT::datatable({
+  output$chart1 <- renderPlotly({
     #Prepare Data frame
-    data <- read.xlsx(here("shinyAppProject1","data", "Backlog Order Summary.xlsx"), colNames = TRUE)
+    data <- read.xlsx(here("data", "Backlog_Master_07-12 Forecast.xlsx"),sheet = 4, colNames = TRUE)
     colnames(data) <- as.character(unlist(data[1,]))
     data <- data[-1, ]
-    if (input$program != "All") {
-      data <- data[data$`Financial Rptg Program` == input$program,]
+    
+    if(input$`P&L` == "All"){
+      chart_data <- data.frame("Category" = unique(data$`P&L`),GP = aggregate(as.numeric(data$GP), by = list(data$`P&L`), FUN = sum)[,2])
+      p <- plot_ly(chart_data, labels = ~Category, values = ~GP, type = 'pie')
     }
-    if (input$`P&L` != "All") {
-      data <- data[data$`P L` == input$`P&L`,]
+    if(input$`P&L` != "All"){
+      chart_data <- data[data$`P&L` == input$`P&L`,]
+      chart_data <- data.frame("Category" = unique(chart_data$`External Rep`),
+                    GP = aggregate(as.numeric(chart_data$GP), by = list(chart_data$`External Rep`), FUN = sum)[,2])
+      p <- plot_ly(chart_data, labels = ~Category, values = ~GP, type = 'pie')
     }
-    if (input$channel != "All") {
-      data <- data[data$`Sales Channel` == input$channel,]
-    }
-    data
-  }))
-  
+    p
+  })
 }
--- shinyAppProject1/ui.R
+++ shinyAppProject1/ui.R
@@ -1,32 +1,27 @@
 library(shiny)
+library(shinydashboard)
+library(plotly)
 # Load the ggplot2 package which provides
 # the 'mpg' dataset.
 library(ggplot2)
 
 fluidPage(
-  titlePanel("Basic DataTable"),
-  
-  # Create a new Row in the UI for selectInputs
-  fluidRow(
-    column(4,
-           selectInput("program",
-                       colnames(data)[2],
-                       c("All",
-                         unique(as.character(data$`Financial Rptg Program`))))
-    ),
-    column(4,
-           selectInput("P&L",
-                       "P&L",
-                       c("All",
-                         unique(as.character(data$`P L`))))
-    ),
-    column(4,
-           selectInput("channel",
-                       "Sales Channel",
-                       c("All",
-                         unique(as.character(data$`Sales Channel`))))
+  dashboardPage(
+    dashboardHeader(title ="BackLog Summary"),
+    dashboardSidebar(),
+    dashboardBody(
+      # Create a new Row in the UI for selectInputs
+        fluidRow(
+          box(
+              plotlyOutput("chart1"),
+              verbatimTextOutput("event"), width = NULL, title = "%GP by P&L"
+          )
+        ),
+        fluidRow(
+          box(
+              selectInput("P&L", "P&L", c("All", unique(as.character((data$`P&L`)))))       
+          )
+        )
     )
-  ),
-  # Create a new row for the table.
-  DT::dataTableOutput("table")
+  )
 )
