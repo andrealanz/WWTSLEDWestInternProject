56736d6f0a66a538153a8ec2d4a89cdbbc499b16
andrealanz <lanza@oregonstate.edu>
2019-08-14 15:50:57 -0700

    add comments

--- shinyAppProject1/server.R
+++ shinyAppProject1/server.R
@@ -4,37 +4,52 @@ library(DT)
 
 function(input, output) {
   
-  # Filter data based on selections
+  #create pie chart output
   output$chart1 <- renderPlotly({
+    #handle if all P&L selected
     if(input$`P&L1` == "All" && input$AM == "none"){
+      #prepare dataframe for plot_ly
       chart_data <- data.frame(
         "Category" = unique(data$`P&L`),
-        GP = aggregate(as.numeric(data$GP), by = list(data$`P&L`), FUN = sum)[,2]
+        #add up GP by P&L
+        GP = aggregate(as.numeric(data$GP), by = list(data$`P&L`), 
+        FUN = sum)[,2]
       )
     }
+    #handle if a P&L is selected with no AM 
     if(input$`P&L1` != "All" && input$AM == "none"){
-      chart_data <- data[data$`P&L` == input$`P&L1`,]
+      chart_data <- data[data$`P&L` == input$`P&L1`,] #get data for specific P&L
+      #prepare dataframe for plot_ly
       chart_data <- data.frame(
         "Category" = unique(chart_data$`External Rep`),
-        GP = aggregate(as.numeric(chart_data$GP), by = list(chart_data$`External Rep`), FUN = sum)[,2]
+        #add GP by AM
+        GP = aggregate(as.numeric(chart_data$GP), by = list(chart_data$`External Rep`), 
+        FUN = sum)[,2]
       )
     }
+    #handle if an AM is selected
     if(input$AM != "none"){
+      #handle if specific AM selected
       if(input$AM != "All"){
-        chart_data <- data[data$`External Rep` == input$AM, ]
+        chart_data <- data[data$`External Rep` == input$AM, ] #get data for specific AM
       }
+      #prepare dataframe for plot_ly
       chart_data <- data.frame(
         "Category" = unique(chart_data$`Ship to Customer`),
-        GP = aggregate(as.numeric(chart_data$GP), by = list(chart_data$`Ship to Customer`), FUN = sum)[,2]
+        #add GP grouped by customer
+        GP = aggregate(as.numeric(chart_data$GP), by = list(chart_data$`Ship to Customer`), 
+        FUN = sum)[,2]
       )
     }
     plot_ly(chart_data, labels = ~Category, values = ~GP, type = 'pie')
   })
   
+  #possible second chart
   output$chart2 <- renderPlotly({
     
   })
   
+  #create AM selection which depends on P&L selection
   output$secondSelection <- renderUI({
     if(input$`P&L1` != "All"){
       chart_data <- data[data$`P&L` == input$`P&L1`,]
@@ -42,6 +57,7 @@ function(input, output) {
     selectizeInput("AM", "AM", c("none","All", unique(as.character((chart_data$`External Rep`)))))
   })
   
+  #create order option selection which will depend on AM selection
   output$thirdSelection <- renderUI({
     selectizeInput("order_options", label = "Order Options", c("none", "% GP", "Status"))
   })
