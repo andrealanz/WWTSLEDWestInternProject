cb613be133fd51d9d05a22118772ea19f9e6c761
Jeremiah Kramer <jeremiahkramer9@gmail.com>
2019-08-21 08:52:35 -0700

    changed it to only convert one excel sheet instead of multiple so it works on front end

--- QuoteReader/csv_reader.py
+++ QuoteReader/csv_reader.py
@@ -43,30 +43,32 @@ def convert_xls_xlsx_to_csv(file):
     wb = xlrd.open_workbook(file)
     # get all possible sheet names (could be more than 1)
     sheets = wb.sheet_names()
-    for sheet in sheets:
-        # check if the sheet is a bad sheet
-        if sheet in bad_sheets:
-            # skip the sheet if it is
-            break
-        # get specific sheet as a Sheet object (needed for sh.nrows below)
-        sh = wb.sheet_by_name(sheet)
-        # head is raw file name without its file extension
-        head, sep, tail = file.partition('.')
-        # append csv file extension to string
-        head += sheet + ".csv"
-        # create csv file with same name
-        csv_file = open(head, 'w', newline='')
-        wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        # write to csv file
-        for rownum in range(sh.nrows):
-            wr.writerow(sh.row_values(rownum))
 
-        csv_file.close()
-        # add this sheet file path to good sheet list
-        good_sheets.append(csv_file.name)
+    # for sheet in sheets: ************ ONLY WITH MULTIPLE SHEETS - DOESN'T WORK ON FRONTEND SO DON'T CONVERT ALL SHEETS
+    # check if the sheet is a bad sheet
+    # if sheets[0] in bad_sheets: *********** ONLY WITH MULTIPLE SHEETS
+    #     # skip the sheet if it is
+    #     break
+    # get specific sheet as a Sheet object (needed for sh.nrows below)
+    sh = wb.sheet_by_name(sheets[0])
+    # head is raw file name without its file extension
+    head, sep, tail = file.partition('.')
+    # append csv file extension to string
+    # head += sheet + ".csv" ******** ONLY WITH MULTIPLE SHEETS
+    head += ".csv"
+    # create csv file with same name
+    csv_file = open(head, 'w', newline='')
+    wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    # write to csv file
+    for rownum in range(sh.nrows):
+        wr.writerow(sh.row_values(rownum))
 
-    # return the array of sheets as csv file (with path)
-    return good_sheets
+    csv_file.close()
+    # add this sheet file path to good sheet list
+    good_sheets.append(csv_file.name)
+
+    # return the array of sheets as csv file (with path) ******ONLY SEND FIRST SHEET SO IT WORKS ON FRONT END
+    return good_sheets[0]
 
 # Utilizes a CSV dictionary to gather fieldnames and reformats the headers on input file to case-desensitize.
 def reformat_header(filename):
@@ -93,150 +95,150 @@ def csv_avt(filename,filepath,vendorname,manufacturername):
         # if the file is NOT .csv, get the array
         new_filepath = convert_xls_xlsx_to_csv(filepath)
 
-    # loop through csv sheet file array (only loops if there's multiple sheets)
-    for file in new_filepath:
-        # update new filepath (from .xls, .xlsx to .csv)
-        filepath = file
-        head, sep, tail = file.partition('/')
-        # filter out the directory name and just get file name
-        filename = tail
+    # loop through csv sheet file array (only loops if there's multiple sheets) *****NOT WOKRING ON FRONT END SO LEAVE COMMENTED
+    # for file in new_filepath:
+    # update new filepath (from .xls, .xlsx to .csv)
+    filepath = new_filepath
+    head, sep, tail = new_filepath.partition('/')
+    # filter out the directory name and just get file name
+    filename = tail
 
-        # counter for total parts in the quote
-        items = 0
-        #variable for vendor quote number found in lines outside of table
-        vendor_quote_found = None
+    # counter for total parts in the quote
+    items = 0
+    #variable for vendor quote number found in lines outside of table
+    vendor_quote_found = None
 
-        # opens input file
-        with open(filepath, 'rt', encoding='utf-8-sig', errors="ignore") as csv_file:
+    # opens input file
+    with open(filepath, encoding='utf-8-sig', errors="ignore") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        #get fieldnames
+        try:
+            csv_reader.fieldnames
+        except UnicodeDecodeError:
+            print("Invalid header")
+            return "Invalid header"
+
+        #delete unnecessary rows (check if second fieldname is blank)
+        while True:
+            count = 0
+            col_names = csv_reader.fieldnames
+            if col_names[0] != "":
+
+                # case-desensitizes and removes punctuation
+                for fieldname in csv_reader.fieldnames:
+                    fieldname = fieldname.lower()
+                    fieldname = fieldname.translate(str.maketrans('','',".,"))
+                    fieldname = fieldname.replace("\n","")
+                    fieldname = fieldname.replace(" ","")
+                    csv_reader.fieldnames[count] = fieldname
+                    count += 1
+                #check if valid header found
+                if part_finder(csv_reader) is not None:
+                    break
+                #handle if vendor quote number found outside of table
+                else:
+                    for field in col_names:
+                        if str_vendorquote_finder(field) != None:
+                            vendor_quote_found = str_vendorquote_finder(field)
+
             csv_reader = csv.DictReader(csv_file)
 
-            #get fieldnames
-            try:
-                csv_reader.fieldnames
-            except UnicodeDecodeError:
-                print("Invalid header")
-                return "Invalid header"
-
-            #delete unnecessary rows (check if second fieldname is blank)
-            while True:
-                count = 0
-                col_names = csv_reader.fieldnames
-                if col_names[0] != "":
-
-                    # case-desensitizes and removes punctuation
-                    for fieldname in csv_reader.fieldnames:
-                        fieldname = fieldname.lower()
-                        fieldname = fieldname.translate(str.maketrans('','',".,"))
-                        fieldname = fieldname.replace("\n","")
-                        fieldname = fieldname.replace(" ","")
-                        csv_reader.fieldnames[count] = fieldname
-                        count += 1
-                    #check if valid header found
-                    if part_finder(csv_reader) is not None:
-                        break
-                    #handle if vendor quote number found outside of table
-                    else:
-                        for field in col_names:
-                            if str_vendorquote_finder(field) != None:
-                                vendor_quote_found = str_vendorquote_finder(field)
-
-                csv_reader = csv.DictReader(csv_file)
-
-            # calls helper functions to find header variations and saves as variable to pass into as dictionary keys
-            part_name = part_finder(csv_reader)
-            description_name = description_finder(csv_reader)
-            listprice_name = listprice_finder(csv_reader)
-            wwtcost_name = wwtprice_finder(csv_reader)
-            quantity_name = quantity_finder(csv_reader)
-            manufacturer_name = manufacturer_finder(csv_reader)
-            vendorquote_name = vendorquote_finder(csv_reader)
-            add_description_name = add_description_finder(csv_reader)
+        # calls helper functions to find header variations and saves as variable to pass into as dictionary keys
+        part_name = part_finder(csv_reader)
+        description_name = description_finder(csv_reader)
+        listprice_name = listprice_finder(csv_reader)
+        wwtcost_name = wwtprice_finder(csv_reader)
+        quantity_name = quantity_finder(csv_reader)
+        manufacturer_name = manufacturer_finder(csv_reader)
+        vendorquote_name = vendorquote_finder(csv_reader)
+        add_description_name = add_description_finder(csv_reader)
 
 
-            # writes into a new file
-            with open('wwt_'  + filename,mode='w',newline='') as wwt_file:
-                # headers for wwt quote template
-                headers = ['Part #', 'Description', 'List Price', 'WWT Cost','Customer Price','Qty','Manufacturer','Vendor','Additional Description', 'Cust Product #', 'Lab Flag (Y/N)', 'Contract Start Date (MM/DD/YYYY)','Contract End Date (MM/DD/YYYY)', 'Serial #', 'Vendor Quote #','Duration','Lead Time', 'Cost Type']
-                # invalid part numbers | stops iterating the loop
-                part_blacklist = ['Products / Services Total', 'Sub-Total', 'Total']
-                # list of text that needs to be filtered out but doesn't stop iterating the loop
-                blacklist_continue_list = ['Hardware:', 'Services:', 'Software:']
-                #creates CSV dictionary writer
-                csv_writer=csv.DictWriter(wwt_file, fieldnames=headers)
-                csv_writer.writeheader()
+        # writes into a new file
+        with open('wwt_'  + filename,mode='w',newline='') as wwt_file:
+            # headers for wwt quote template
+            headers = ['Part #', 'Description', 'List Price', 'WWT Cost','Customer Price','Qty','Manufacturer','Vendor','Additional Description', 'Cust Product #', 'Lab Flag (Y/N)', 'Contract Start Date (MM/DD/YYYY)','Contract End Date (MM/DD/YYYY)', 'Serial #', 'Vendor Quote #','Duration','Lead Time', 'Cost Type']
+            # invalid part numbers | stops iterating the loop
+            part_blacklist = ['Products / Services Total', 'Sub-Total', 'Total']
+            # list of text that needs to be filtered out but doesn't stop iterating the loop
+            blacklist_continue_list = ['Hardware:', 'Services:', 'Software:']
+            #creates CSV dictionary writer
+            csv_writer=csv.DictWriter(wwt_file, fieldnames=headers)
+            csv_writer.writeheader()
 
-                # iterates through rows of csv_reader dictionary object
-                for i, row in enumerate(csv_reader):
-                    # items is a counter for parts with a description field (doesn't work for every quote)
-                    #if row[description_name]:
-                    #   items += 1
-                    # if the iterator is greater than or equal to the items counter, stop iterating
-                    # if i >= (items):
-                        # break
+            # iterates through rows of csv_reader dictionary object
+            for i, row in enumerate(csv_reader):
+                # items is a counter for parts with a description field (doesn't work for every quote)
+                #if row[description_name]:
+                #   items += 1
+                # if the iterator is greater than or equal to the items counter, stop iterating
+                # if i >= (items):
+                    # break
 
-                    # terminates when rows are not populated by part #, description and quantity
-                    if row[part_name] == '' and row[description_name] == '' and row[quantity_name] == '':
-                        print("row doesn't have part #, description, or quantity")
-                        break
+                # terminates when rows are not populated by part #, description and quantity
+                if row[part_name] == '' and row[description_name] == '' and row[quantity_name] == '':
+                    print("row doesn't have part #, description, or quantity")
+                    break
 
-                    # terminates with incorrect part #
-                    if row[part_name] in part_blacklist:
-                        print("found blacklisted item, stopping loop")
-                        break
+                # terminates with incorrect part #
+                if row[part_name] in part_blacklist:
+                    print("found blacklisted item, stopping loop")
+                    break
 
-                    # skips line with certain strings 
-                    if row[part_name] in blacklist_continue_list:
-                        print("filtered out blacklisted item")
-                        continue
+                # skips line with certain strings
+                if row[part_name] in blacklist_continue_list:
+                    print("filtered out blacklisted item")
+                    continue
 
-                    #initializes empty dictionary to add keys-value pairs into
-                    output_dictionary = {}
-                    # Updates individual columns for each row into dictionary. If the column does not exist on original input file, leaves blank entry.
-                    # updates Part #
-                    if part_name is not None:
-                        output_dictionary.update({'Part #':row[part_name]})
-                    else:
-                        output_dictionary.update({'Part #':None})
-                    #updates Description
-                    if description_name is not None:
-                        output_dictionary.update({'Description':row[description_name]})
-                    else:
-                        output_dictionary.update({'Description':None})
-                    # updates List Price
-                    if listprice_name is not None:
-                        output_dictionary.update({'List Price':row[listprice_name]})
-                    else:
-                        output_dictionary.update({'List Price':None})
-                    # updates WWT Cost
-                    if wwtcost_name is not None:
-                        output_dictionary.update({'WWT Cost':row[wwtcost_name]})
-                    else:
-                        output_dictionary.update({'WWT Cost':None})
-                    # updates Qty
-                    if quantity_name is not None:
-                        output_dictionary.update({'Qty':row[quantity_name]})
-                    else:
-                        output_dictionary.update({'Description':None})
-                    # updates Manufacturer
-                    if manufacturer_name is not None:
-                        output_dictionary.update({'Manufacturer':row[manufacturer_name]})
-                    else:
-                        output_dictionary.update({'Manufacturer':manufacturername})
-                    # updates Vendor Quote #
-                    if vendor_quote_found != None:
-                        output_dictionary.update({'Vendor Quote #':vendor_quote_found})
-                    elif vendorquote_name is not None:
-                        output_dictionary.update({'Vendor Quote #':row[vendorquote_name]})
-                    else:
-                        output_dictionary.update({'Vendor Quote #':None})
+                #initializes empty dictionary to add keys-value pairs into
+                output_dictionary = {}
+                # Updates individual columns for each row into dictionary. If the column does not exist on original input file, leaves blank entry.
+                # updates Part #
+                if part_name is not None:
+                    output_dictionary.update({'Part #':row[part_name]})
+                else:
+                    output_dictionary.update({'Part #':None})
+                #updates Description
+                if description_name is not None:
+                    output_dictionary.update({'Description':row[description_name]})
+                else:
+                    output_dictionary.update({'Description':None})
+                # updates List Price
+                if listprice_name is not None:
+                    output_dictionary.update({'List Price':row[listprice_name]})
+                else:
+                    output_dictionary.update({'List Price':None})
+                # updates WWT Cost
+                if wwtcost_name is not None:
+                    output_dictionary.update({'WWT Cost':row[wwtcost_name]})
+                else:
+                    output_dictionary.update({'WWT Cost':None})
+                # updates Qty
+                if quantity_name is not None:
+                    output_dictionary.update({'Qty':row[quantity_name]})
+                else:
+                    output_dictionary.update({'Description':None})
+                # updates Manufacturer
+                if manufacturer_name is not None:
+                    output_dictionary.update({'Manufacturer':row[manufacturer_name]})
+                else:
+                    output_dictionary.update({'Manufacturer':manufacturername})
+                # updates Vendor Quote #
+                if vendor_quote_found != None:
+                    output_dictionary.update({'Vendor Quote #':vendor_quote_found})
+                elif vendorquote_name is not None:
+                    output_dictionary.update({'Vendor Quote #':row[vendorquote_name]})
+                else:
+                    output_dictionary.update({'Vendor Quote #':None})
 
-                    #updates additional description and vendor fields
-                    if add_description_name is not None:
-                        output_dictionary.update({'Additional Description':row[add_description_name], 'Vendor':vendorname})
-                    else:
-                        output_dictionary.update({'Additional Description':None, 'Vendor':vendorname})
+                #updates additional description and vendor fields
+                if add_description_name is not None:
+                    output_dictionary.update({'Additional Description':row[add_description_name], 'Vendor':vendorname})
+                else:
+                    output_dictionary.update({'Additional Description':None, 'Vendor':vendorname})
 
-                    csv_writer.writerow(output_dictionary)
+                csv_writer.writerow(output_dictionary)
 
 
 # Looks for variations of 'Part #' fieldnames and returns value found in vendor csv quote. Returns None if no variation is found.
@@ -376,7 +378,7 @@ def add_description_finder(csv_dict):
 
 # csv_avt('061219-WWT-Hawaii Medical Service Association.xls', 'quotes/061219-WWT-Hawaii Medical Service Association.xls', "test", "test")
 # csv_avt('06062019-WWT-Hawaii Medical Service Association[1].xls', 'quotes/06062019-WWT-Hawaii Medical Service Association[1].xls', "test", "test")
-# csv_avt('PaloAlto_PAN_Hawaiian Airlines_0020706101.xls', 'quotes/PaloAlto_PAN_Hawaiian Airlines_0020706101.xls', "test", "test")
+csv_avt('PaloAlto_PAN_Hawaiian Airlines_0020706101.xls', 'quotes/PaloAlto_PAN_Hawaiian Airlines_0020706101.xls', "test", "test")
 # csv_avt('PAN_Brigham Young University-Hawaii_0020724391.xls', 'quotes/PAN_Brigham Young University-Hawaii_0020724391.xls', "test", "test")
 # csv_avt('QUO-1953529-L6W1V2-1.xlsx', 'quotes/QUO-1953529-L6W1V2-1.xlsx', "test", "test")
 # csv_avt('QUO-2621751-L9R4M7-0.xlsx', 'quotes/QUO-2621751-L9R4M7-0.xlsx', "test", "test")
